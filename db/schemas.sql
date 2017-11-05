781b8dda0f318de20825efdad44b9ebf693be5e3
Andrew Kelley <iodine73@gmail.com>
2017-11-05 14:59:36 -0500

    db and config folders added

---
+++ db/schemas.sql
@@ -0,0 +1,29 @@
+CREATE database podtalk_db;
+
+use podtalk_db;
+
+CREATE TABLE users (
+	userID INT AUTO_INCREMENT NOT NULL, 
+	username VARCHAR(50) NOT NULL,
+	firstname VARCHAR(50) NOT NULL,
+	lastname VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	PRIMARY KEY(userID)
+);
+
+CREATE TABLE comments (
+	commentID INT AUTO_INCREMENT NOT NULL, 
+    userID INT AUTO_INCREMENT NOT NULL,
+	body VARCHAR(255) NOT NULL,
+	PRIMARY KEY(commentID)
+);
+
+CREATE TABLE podcast (
+	podcastID INT AUTO_INCREMENT NOT NULL, 
+	podcastURL VARCHAR(255) NOT NULL,
+    podcastTitle VARCHAR(50) NOT NULL,
+	userID INT AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY(podcastID)
+);
+
