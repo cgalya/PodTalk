a7e48c231af646cfa20a6a76d80bcb8162be0fb5
Claire Galyaa <claire.galya@gmail.com>
2017-11-22 12:03:32 -0500

    header

--- client/src/partials/Header.js
+++ client/src/partials/Header.js
@@ -1,1 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
+
+const Header = (props) => (
+  <div className="header">
+    <div className="left">
+      <img src="" alt=""/>
+    </div>
+    <div className="right">
+      {props.children}
+    </div>
+  </div>
+)
